7b886d17368e8e33dea5813c73048f07345821df
Nessatunes <essatunes@gmail.com>
2024-05-22 00:15:27 -0300

    a

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'https://codeburger-api-production-da71.up.railway.app',
+  baseURL: 'https://codeburger-api-production-da71.up.railway.app/',
 })
 
 apiCodeBurger.interceptors.request.use(async (config) => {
