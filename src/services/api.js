6fc39ec4bee8b9518c90a189c838b7c24a6d50ab
Nessatunes <essatunes@gmail.com>
2024-05-22 21:12:19 -0300

    ajustes

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,16 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-undef */
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'https://codeburger-api-production-da71.up.railway.app/',
+  baseURL: 'https://codeburger-api-production-da71.up.railway.app',
+  withCredentials: true,
 })
 
 apiCodeBurger.interceptors.request.use(async (config) => {
   const userData = await localStorage.getItem('codeburger:userData')
   const token = userData && JSON.parse(userData).token
-  config.headers.Authorization = `Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 })
 
